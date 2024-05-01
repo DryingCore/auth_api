22256626b83e0b2ad177cf4a01ede04dbd4cbce5
DryingCore <lein0rb4g@gmail.com>
2024-05-01 20:08:36 -0300

    modified:   Server.js modified:   routes/Auth.js new file:   utils/Validation.js

--- routes/Auth.js
+++ routes/Auth.js
@@ -1,55 +1,31 @@
 // Needed imports
-// -------------------------------------
 const router = require("express").Router();
 const User = require("../models/User.js"); // Importing User Schema
-const Joi = require("joi")
-// -------------------------------------
-
-// Validation Schema
-// -----------------------------------------------
-const schema = Joi.object({
-    name: Joi.string().min(6).required(),
-    email: Joi.string().min(6).email().required(),
-    password: Joi.string().min(6).required()
-});
-// -----------------------------------------------
-
+const { registerValidation } = require("../utils/Validation.js")
 
 // Routes
-// -------------------------------------------------------------
 router.post("/register", async (req, res) => {
-    // Validate user input
-    // -----------------------------------------------------------
-    const { error } = schema.validate(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-    // -----------------------------------------------------------
-
+    // Validating user input
+    const { error } = registerValidation(req.body)
+    if (error) return res.status(400).send(error.details[0].message);
 
     // Create a new User instance
-    // -------------------------------
     const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
         date: req.body.date
     });
-    // -------------------------------
-
 
     // Error handling
-    // ---------------------------------------
     try {
-        // Save the user to the database
-        const savedUser = await user.save();
-        res.status(200).send("Success");
+        const savedUser = await user.save(); // Save the user to the database
+        res.status(200).send("Saved new user...");
         console.log(savedUser);
     } catch (error) {
         res.status(400).send(error);
+        console.log(error)
     }
-    // ---------------------------------------
 });
-// -------------------------------------------------------------
 
 module.exports = router;
